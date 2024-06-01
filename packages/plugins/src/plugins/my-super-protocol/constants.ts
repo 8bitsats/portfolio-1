5f6709ca961453a25a6d41e9a8b0a014e1d2728e
8 bit <8bit@ordlibrary.com>
2024-06-01 01:55:01 -0400

    ff

---
+++ packages/plugins/src/plugins/my-super-protocol/constants.ts
@@ -0,0 +1,11 @@
+import { Platform } from '@sonarwatch/portfolio-core';
+
+export const platformId = 'my-super-protocol';
+export const platform: Platform = {
+  id: platformId,
+  name: 'My Super Protocol',
+  image: 'https://sonar.watch/img/platforms/my-super-protocol.webp',
+  // defiLlamaId: 'foo-finance', // from https://defillama.com/docs/api
+  // website: 'https://myplatform.com',
+  // twitter: 'https://twitter.com/myplatform',
+};
---
+++ packages/plugins/src/plugins/portfolio/constants.ts
@@ -0,0 +1,11 @@
+import { Platform } from '@sonarwatch/portfolio-core';
+
+export const platformId = 'portfolio';
+export const platform: Platform = {
+  id: platformId,
+  name: 'Portfolio',
+  image: 'https://sonar.watch/img/platforms/portfolio.webp',
+  // defiLlamaId: 'foo-finance', // from https://defillama.com/docs/api
+  // website: 'https://myplatform.com',
+  // twitter: 'https://twitter.com/myplatform',
+};
